1ca8518b47c4e82d5d4075ac69c8b88dd82814c5
shubham-codes568 <shubham.100568@gmail.com>
2026-06-06 21:36:03 +0530

    Day 10
    Summer Assignment Day 10

---
+++ day 10/2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int i, j, rows;
+    printf("enter the number of rows:");
+    scanf("%d",&rows);
+    for(i=1; i<=rows; i++){
+        for(j=1; j<=i-1; j++){
+            printf(" ");
+        }
+        for(j=1; j<=2*(rows-i)+1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
