b1ac5f6af088973e83b91b272d3e09819050f134
shubham-codes568 <shubham.100568@gmail.com>
2026-06-23 23:12:46 +0530

    Day 27

---
+++ day 27/3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int empId;
+    char name[50];
+    float basic, hra, da, gross;
+
+    printf("Enter Employee ID: ");
+    scanf("%d", &empId);
+
+    printf("Enter Employee Name: ");
+    scanf("%s", name);
+
+    printf("Enter Basic Salary: ");
+    scanf("%f", &basic);
+
+    hra = basic * 0.20;
+    da  = basic * 0.10;
+
+    gross = basic + hra + da;
+
+    printf("\n--- Salary Details ---\n");
+    printf("Employee ID : %d\n", empId);
+    printf("Name        : %s\n", name);
+    printf("Basic Salary: %.2f\n", basic);
+    printf("HRA         : %.2f\n", hra);
+    printf("DA          : %.2f\n", da);
+    printf("Gross Salary: %.2f\n", gross);
+
+    return 0;
+}
---
+++ day 27/4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[50];
+    int roll;
+    float m1, m2, m3, m4, m5;
+    float total, percentage;
+
+    printf("Enter Student Name: ");
+    scanf("%s", name);
+
+    printf("Enter Roll Number: ");
+    scanf("%d", &roll);
+
+    printf("Enter Marks of 5 Subjects:\n");
+    scanf("%f %f %f %f %f", &m1, &m2, &m3, &m4, &m5);
+
+    total = m1 + m2 + m3 + m4 + m5;
+    percentage = total / 5;
+
+    printf("\n----- MARKSHEET -----\n");
+    printf("Name       : %s\n", name);
+    printf("Roll No    : %d\n", roll);
+    printf("Total Marks: %.2f\n", total);
+    printf("Percentage : %.2f%%\n", percentage);
+
+    if(percentage >= 90)
+        printf("Grade: A+\n");
+    else if(percentage >= 75)
+        printf("Grade: A\n");
+    else if(percentage >= 60)
+        printf("Grade: B\n");
+    else if(percentage >= 40)
+        printf("Grade: C\n");
+    else
+        printf("Grade: Fail\n");
+
+    return 0;
+}
